2818eb1bea1f9b484115351f80a36d057eba0cbf
David R <codingaccelerator@gmail.com>
2022-10-08 12:02:36 +0200

    ♻️ F01: LOGIN > Create LoginForm

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,25 +1,24 @@
 import { useState } from "react";
 
-export default function LoginPage() {
-    
-  const [inputValue, setInputValue] = useState("")
+export default function LoginForm() {
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    alert(`Bonjour ${inputValue}`)
-    setInputValue("")
-  }
+    const [inputValue, setInputValue] = useState('');
 
-  const handleChange = (event) => { 
-    setInputValue(event.target.value)  
-  }
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      alert(`Bonjour ${inputValue}`);
+      setInputValue('');
+    };
+
+    const handleChange = (event) => {
+      setInputValue(event.target.value);
+    };
 
   return (
-    <div>
+      <form action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
       <br />
       <h2>Connectez-vous</h2>
-      <form action="submit" onSubmit={handleSubmit}>
         <input
           value={inputValue}
           onChange={handleChange}
@@ -29,6 +28,5 @@ export default function LoginPage() {
           />
         <button>Accéder à votre espace</button>
       </form>
-    </div>
   );
 }
