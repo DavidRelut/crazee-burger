420f88c1594ed81a369b17bb21a796713efaed48
David R <codingaccelerator@gmail.com>
2022-11-12 03:32:49 +0100

    💄 F03: Added Logo + style & Resize login card

--- src/components/pages/login/LoginForm.js
+++ src/components/pages/login/LoginForm.js
@@ -1,8 +1,9 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components/macro"
-import background_img from '../../../images/burger_background.jpg';
 import { theme } from "../../../theme";
+import background_img from '../../../images/burger_background.jpg';
+import logo from '../../../images/logo-orange.png';
 import { BsPersonCircle } from 'react-icons/bs';
 
 export default function LoginForm() {
@@ -22,12 +23,15 @@ export default function LoginForm() {
 
   return (
     <LoginFormStyled action="submit" onSubmit={handleSubmit}>
+      <h1 className="title font_amatic-sc">
+        Crazee <div className="logo"></div> Burger
+      </h1>
       <div className="login-card">
         <h1 className="global_text font_amatic-sc">Bienvenue chez nous !</h1>
         <hr />
         <h2 className="global_text font_amatic-sc">Connectez-vous</h2>
         <div className="input-card">
-          <BsPersonCircle className="bsPersonCircle"/>
+          <BsPersonCircle className="bsPersonCircle" />
           <input
             value={name}
             onChange={handleChange}
@@ -37,7 +41,7 @@ export default function LoginForm() {
             className="font_open-sans"
           />
         </div>
-        <button className="font_open-sans btn">Accéder à votre espace</button>
+        <button className="font_open-sans btn">Accéder à mon espace</button>
       </div>
     </LoginFormStyled>
   );
@@ -53,16 +57,38 @@ const LoginFormStyled = styled.form`
   background-repeat: no-repeat;
 
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
+  .title {
+    color: ${theme.colors.primary_burger};
+    font-size: ${theme.fonts.P7};
+    display: inline-flex;
+    justify-content: space-evenly;
+    align-items: center;
+    letter-spacing: ${theme.spacing.xxs};
+    font-weight: ${theme.weights.bold};
+  }
+
+  .logo {
+    width: 170px;
+    height: 170px;
+    background-image: url(${logo});
+    background-position: center;
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: 0 10px;
+  }
+
   .login-card {
-    width: 300px;
+    width: 430px;
     height: 40%;
     display: flex;
     flex-direction: column;
     justify-content: space-evenly;
     align-items: center;
+    margin-bottom: 70px;
   }
 
   .global_text {
@@ -72,27 +98,28 @@ const LoginFormStyled = styled.form`
   h1 {
     margin: 0;
     padding: 0;
-    font-size: ${theme.fonts.P4};
+    font-size: ${theme.fonts.P5};
   }
 
   h2 {
-    font-size: ${theme.fonts.P3Bis};
-    margin: 10px 0 0 0;
+    font-size: ${theme.fonts.P4};
+    margin: 20px 0 10px 0;
   }
 
   hr {
     width: 100%;
     background: ${theme.colors.primary_burger2};
-    border: 1px solid ${theme.colors.primary_burger2};
+    border: 1.2px solid ${theme.colors.primary_burger2};
     border-radius: 0.5px;
   }
 
   .input-card {
+    background-color: ${theme.colors.white};
     display: inline-flex;
     justify-content: initial;
     align-items: center;
     width: 100%;
-    height: 2.5rem;
+    height: 3rem;
   }
 
   .bsPersonCircle {
@@ -101,16 +128,10 @@ const LoginFormStyled = styled.form`
   }
 
   input {
-    width: 14rem;
+    width: 83%;
     border-radius: 5px;
     border-style: none;
     outline: none;
-  }
-
-  .input-card {
-    background-color: white;
-    display: inline-flex;
-    width: 100%;
   }
 
   input::placeholder {
@@ -118,7 +139,7 @@ const LoginFormStyled = styled.form`
     font-size: ${theme.fonts.XS};
     padding-left: 1px;
   }
-  
+
   input:focus::placeholder {
     border: 2px solid ${theme.colors.blue};
     border-radius: ${theme.borderRadius.round};
@@ -126,12 +147,13 @@ const LoginFormStyled = styled.form`
 
   button {
     width: 100%;
-    height: 2.5rem;
+    height: 3rem;
     border-radius: ${theme.borderRadius.round};
     border-style: none;
     color: ${theme.colors.white};
     background-color: ${theme.colors.primary};
     cursor: pointer;
+    margin-top: 10px;
   }
 
   .btn {
