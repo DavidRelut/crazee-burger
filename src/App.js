5b5a7c3dddf110ceb99c151d80601086ecebe734
David R <codingaccelerator@gmail.com>
2022-11-30 14:41:50 +0100

    🐛 F03: Delete unnecessary code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import LoginPage from './components/pages/Login/LoginPage';
 import { Routes, Route } from 'react-router-dom';
 import OrderPage from './components/pages/Order/OrderPage';
 import ErrorPage from './components/pages/Error/ErrorPage';
-import './App.css';
 
 function App() {
   return (
