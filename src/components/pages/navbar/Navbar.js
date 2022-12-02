7de2ca3e62582f0db50d970b189bc7550e55e872
David R <codingaccelerator@gmail.com>
2022-12-02 21:29:12 +0100

    💄 F04:  NAVBAR > OrderPage > Create <Navbar/>

---
+++ src/components/pages/navbar/Navbar.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+import { theme } from '../../../theme';
+import Logo from '../reusable-ui/Logo';
+import Profile from './Profile';
+
+export default function Navbar({ name }) {
+  return (
+    <NavbarStyled>
+      <div className="logo">
+        <Logo />
+      </div>
+      <Profile name={name} />
+    </NavbarStyled>
+  );
+}
+
+const NavbarStyled = styled.div`
+  height: 10%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  border: 1px transparent black;
+  border-radius: ${theme.borderRadius.extraRound}
+  ${theme.borderRadius.extraRound} 0px 0px;
+  background: ${theme.colors.white};
+  box-shadow: -1px 9px 13px -1px rgba(0, 0, 0, 0.2);
+
+  .logo {
+    cursor: pointer;
+    transform: scale(0.4);
+    margin-left: 25px;
+  }
+`;
