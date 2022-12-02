d783205dc1596edde847c10c1c400a14b04fafd5
David R <codingaccelerator@gmail.com>
2022-12-02 21:30:13 +0100

    💄 F04:  NAVBAR > OrderPage > Create <Profile/>

--- src/components/pages/order/OrderPage.js
+++ src/components/pages/order/OrderPage.js
@@ -1,15 +1,36 @@
-import { Link, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { theme } from '../../../theme';
+import Navbar from '../navbar/Navbar';
+
 
 export default function OrderPage() {
 
   const {name} = useParams()
   
   return (
-      <div>
-          <h1>Bonjour {name}</h1>
-          <Link to={'/'}>
-              <button>Déconnexion</button>
-          </Link>
+    <OrderPageSyled>
+      <div className='container'>
+        <Navbar name={name} />
       </div>
+    </OrderPageSyled>
   );
 }
+
+const OrderPageSyled = styled.div`
+  background: ${theme.colors.primary};
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  justify-content: center;
+
+
+  .container {
+    /* background-color: ${theme.colors.background_white}; */
+    border-radius: ${theme.borderRadius.extraRound};
+    margin: ${theme.spacing.md};
+    width: 100%;
+    max-width: 1400px;
+  }
+`;
