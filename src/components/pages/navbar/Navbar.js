82a8555802f5f67fc918e2fe44b810105d879d39
David R <codingaccelerator@gmail.com>
2022-12-04 15:44:19 +0100

    ♻️ F04: ORDER > Navbar > Update global padding + border top

--- src/components/pages/navbar/Navbar.js
+++ src/components/pages/navbar/Navbar.js
@@ -6,31 +6,25 @@ import Profile from './Profile';
 export default function Navbar({ name }) {
   return (
     <NavbarStyled>
-      <div className="logo">
-        <Logo />
-      </div>
+      <Logo />
       <Profile name={name} />
     </NavbarStyled>
   );
 }
 
 const NavbarStyled = styled.nav`
-  height: 10%;
+  height: 10vh;
 
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
 
-  border: 1px transparent black;
-  border-radius: ${theme.borderRadius.extraRound}
-  ${theme.borderRadius.extraRound} 0px 0px;
+  padding: 0 70px 0 25px;
+
+  border-bottom: 1px solid ${theme.colors.greyLight};
   background: ${theme.colors.white};
   box-shadow: -1px 9px 13px -1px rgba(0, 0, 0, 0.2);
-
-  .logo {
-    cursor: pointer;
-    transform: scale(0.4);
-    margin-left: 25px;
-  }
+  border-top-left-radius: ${theme.borderRadius.extraRound};
+  border-top-right-radius: ${theme.borderRadius.extraRound};
 `;
