3c18ee74e74d2b6cac6d8ffb13bd1b88384324c6
David R <codingaccelerator@gmail.com>
2022-12-09 14:56:24 +0100

    ♻️ F05: ORDER > MAIN > Add <Menu/>

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -1,29 +1,27 @@
-import styled from "styled-components";
-import { theme } from "../../../../theme";
+import styled from 'styled-components';
+import { theme } from '../../../../theme';
+import Menu from './Menu/Menu';
 
 export default function Main() {
   return (
     <MainStyled>
       <div className="basket">Basket</div>
-      <div className="menu">Menu</div>
+      <Menu />
     </MainStyled>
-  )
+  );
 }
 
 const MainStyled = styled.div`
-background-color: ${theme.colors.background_white};
-flex: 1;
-box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-border-bottom-left-radius: ${theme.borderRadius.extraRound};
-border-bottom-right-radius: ${theme.borderRadius.extraRound};
+  background-color: ${theme.colors.background_white};
+  flex: 1;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  border-bottom-left-radius: ${theme.borderRadius.extraRound};
+  border-bottom-right-radius: ${theme.borderRadius.extraRound};
 
-display: grid;
-grid-template-columns: 25% 1fr;
+  display: grid;
+  grid-template-columns: 25% 1fr;
 
   .basket {
     background: ${theme.colors.incognito};
-  }
-  .menu {
-    background: ${theme.colors.green};
   }
 `;
