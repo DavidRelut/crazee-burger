afc5a7178c1855b88f75d550eb102ab3f7980089
David R <codingaccelerator@gmail.com>
2022-12-09 15:48:15 +0100

    💄 F05: ORDER > MENU > Add final style

--- src/components/pages/order/Main/Menu/Menu.js
+++ src/components/pages/order/Main/Menu/Menu.js
@@ -17,12 +17,13 @@ export default function MenuCard() {
 }
 
 const MenuStyled = styled.div`
-  background: ${theme.colors.redSecondary};
+  background: ${theme.colors.background_white};
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
   .product {
     background: ${theme.colors.incognito};
