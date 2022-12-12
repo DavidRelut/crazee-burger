1ea8be0335ad185f4dbf3a02f88b2997eb3df867
David R <codingaccelerator@gmail.com>
2022-12-12 09:28:48 +0100

    ♻️ F05: MAIN > PrimaryButton > Add theme

--- src/components/reusable-ui/PrimaryButton.js
+++ src/components/reusable-ui/PrimaryButton.js
@@ -12,7 +12,7 @@ export default function PrimaryButton({ label, Icon, className }) {
 
 const PrimaryButtonStyled = styled.button`
   width: 100%;
-  border: 1px solid red;
+  border: 1px solid {theme.colors.greyLight};
   display: inline-flex;
   justify-content: center;
   align-items: center;
@@ -22,12 +22,12 @@ const PrimaryButtonStyled = styled.button`
   line-height: 1;
 
   padding: 18px 24px;
-  border-radius: 5px;
-  font-size: 15px;
-  font-weight: 800;
-  color: white;
-  background-color: #ff9f1b;
-  border: 1px solid #ff9f1b;
+  border-radius: ${theme.borderRadius.round};
+  font-size: ${theme.fonts.P0};
+  font-weight: ${theme.weights.bold};
+  color: ${theme.colors.white};
+  background-color: ${theme.colors.primary_burger};
+  border: 1px solid ${theme.colors.primary_burger};
 
   :hover {
     color: ${theme.colors.primary};
@@ -47,7 +47,7 @@ const PrimaryButtonStyled = styled.button`
   }
 
   &.with-focus {
-    border: 1px solid white;
+    border: 1px solid ${theme.colors.white};
     background-color: ${theme.colors.white};
     color: ${theme.colors.primary};
     :hover {
