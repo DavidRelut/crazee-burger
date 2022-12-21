6b0a4a3e25579496c0d62a4b97ef99494b0f14de
David R <codingaccelerator@gmail.com>
2022-12-21 18:47:37 +0100

    ♻️ F06: NAVBAR > ToastAdmin > Simplify  the Toast container

--- src/components/pages/order/Navbar/ToastAdmin.js
+++ src/components/pages/order/Navbar/ToastAdmin.js
@@ -5,13 +5,11 @@ import styled from "styled-components";
 
 export default function ToastAdmin() {
   return (
-    <ToastAdminStyled>
-      <ToastContainer className="toaster" bodyClassName="body-toast" />
-    </ToastAdminStyled>
+    <ToastContainerStyled className="toaster" bodyClassName="body-toast" />
   );
 }
 
-const ToastAdminStyled = styled.div`
+const ToastContainerStyled = styled(ToastContainer)`
   .toaster {
     max-width: 300px;
   }
