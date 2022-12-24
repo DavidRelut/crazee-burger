4e815e4ae9f637616e69688f425f9dfcacdde413
David R <codingaccelerator@gmail.com>
2022-12-24 14:12:29 +0100

    💄 F07: MENU > Add specific position for <PanelContainer />

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -1,9 +1,10 @@
-import { useState } from 'react';
-import styled from 'styled-components';
-import { fakeMenu2 } from '../../../../fakeData/fakeMenu';
-import { theme } from '../../../../theme/index';
-import { formatPrice } from '../../../../utils/maths';
-import Card from '../../../reusable-ui/Card';
+import { useState } from "react";
+import styled from "styled-components";
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { theme } from "../../../../theme/index";
+import { formatPrice } from "../../../../utils/maths";
+import Card from "../../../reusable-ui/Card";
+import PanelContainer from "./Panel/PanelContainer";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
@@ -11,23 +12,35 @@ export default function Menu() {
   return (
     <MenuStyled>
       {menu.map(({ id, title, imageSource, price }) => {
-        return <Card
+        return (
+          <Card
             key={id}
             title={title}
             imageSource={imageSource}
             leftDescription={formatPrice(price)}
-          />      
+          />
+        );
       })}
+      <PanelContainer className="panel-container" />
     </MenuStyled>
   );
 }
 
 const MenuStyled = styled.div`
+  position: relative;
   background: ${theme.colors.background_white};
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  .panel-container {
+    position: absolute;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    z-index: 2;
+  }
 `;
