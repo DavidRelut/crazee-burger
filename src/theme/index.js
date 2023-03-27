c5460e168769dfbc6dd405df71e3550ad15a32ba
DavidR <codingaccelerator@gmail.com>
2023-03-27 21:32:23 +0200

    💄 F07: Add new color

--- src/theme/index.js
+++ src/theme/index.js
@@ -10,6 +10,7 @@ const colors = {
   red: "#e25549",
   redSecondary: "#c4151c",
   blue: "#51a7e1",
+  greyExtraLight: "#D3D3D3",
   greyLight: "#e4e5e9",
   greyMedium: "#a7a8ad",
   greySemiDark: "#93a2b1",
