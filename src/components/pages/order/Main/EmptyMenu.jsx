6e740353f9be8b8c4ad83361ff7efe4a07bb7e71
DavidR <codingaccelerator@gmail.com>
2023-03-31 15:32:28 +0200

    ✨F08: MENU > EMPTY > Create <EmptyMenu /> to generate new product

---
+++ src/components/pages/order/Main/EmptyMenu.jsx
@@ -0,0 +1,55 @@
+import { theme } from "../../../../theme";
+import PrimaryButton from "../../../reusable-ui/PrimaryButton";
+import styled from "styled-components";
+
+export default function EmptyMenu({ onReset }) {
+  return (
+    <EmptyMenuStyled>
+      <span className="txt-info-1">Le menu est vide ?</span>
+      <span className="txt-info-2">
+        Cliquez ci-dessous pour le réinitialiser
+      </span>
+      <PrimaryButton
+        label="Générer de nouveaux produits"
+        className="reset-btn"
+        onClick={onReset}
+      />
+    </EmptyMenuStyled>
+  );
+}
+
+const EmptyMenuStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+
+  .txt-info-1 {
+    font-family: "Amatic SC";
+    font-weight: 700;
+    font-size: 36px;
+    color: ${theme.colors.greyBlue};
+    text-transform: uppercase;
+  }
+
+  .txt-info-2 {
+    font-family: "Amatic SC";
+    font-weight: 400;
+    font-size: 36px;
+    margin-top: 20px;
+    color: ${theme.colors.greyBlue};
+    text-transform: uppercase;
+    margin: 30px 0;
+  }
+
+  .reset-btn {
+    padding: 20px;
+    font-size: ${theme.fonts.size.XS};
+    padding: 19px 25px 19px 25px;
+    width: 225px;
+    height: 50px;
+    cursor: pointer;
+    margin-bottom: 120px;
+  }
+`;
