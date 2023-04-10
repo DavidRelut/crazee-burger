b0bb4c334ef7fa793c12cac7c908767f2ab843eb
DavidR <codingaccelerator@gmail.com>
2023-04-10 14:47:22 +0200

    ✨ 💄 F09: ADMIN > EDIT > EMPTY EDIT > Create <EmptyEditProduct />

---
+++ src/components/pages/order/Main/Admin/AdminPanel/EditProduct/EmptyEditProduct.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { HiCursorClick } from "react-icons/hi";
+import styled from "styled-components";
+import { theme } from "../../../../../../../theme";
+
+export default function EmptyEditProduct() {
+  return (
+    <EmptyEditProductStyled>
+      <span className="edit-message">
+        Cliquer sur un produit pour le modifier
+      </span>
+      <HiCursorClick />
+    </EmptyEditProductStyled>
+  );
+}
+
+const EmptyEditProductStyled = styled.div`
+  font-size: ${theme.fonts.size.P3};
+  color: ${theme.colors.greyBlue};
+  font-family: "Amatic SC";
+  font-weight: ${theme.fonts.weights.regular};
+  display: flex;
+  align-items: center;
+  margin: 85px 0 0 50px;
+
+  .edit-message {
+    margin-right: 10px;
+  }
+`;
