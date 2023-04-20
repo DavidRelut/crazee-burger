4f0082d9eba2dcc63374cda1b4cc5c91673a7aee
DavidR <codingaccelerator@gmail.com>
2023-04-20 09:10:36 +0200

    ♻️ F09: ADMIN > EDIT > Extract EditInfoMessage

---
+++ src/components/pages/order/Main/Admin/AdminPanel/EditProduct/EditInfoMessage.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+import { theme } from "../../../../../../../theme";
+
+export default function EditInfoMessage() {
+  return (
+    <EditInfoMessageStyled>
+      <span className="edit-text">
+        Cliquer sur un produit du menu pour le modifier
+        <span className="underline">en temps réel</span>
+      </span>
+    </EditInfoMessageStyled>
+  );
+}
+
+const EditInfoMessageStyled = styled.div`
+  .edit-text {
+    grid-area: 3 / 2 / 4 / -1;
+    font-size: ${theme.fonts.size.SM};
+    color: ${theme.colors.primary};
+    .underline {
+      margin-left: 5px;
+      text-decoration: underline;
+    }
+  }
+`;
--- src/components/pages/order/Main/Admin/AdminPanel/EditProduct/EditProduct.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditProduct/EditProduct.jsx
@@ -5,6 +5,7 @@ import OrderContext from "../../../../../../../context/OrderContext";
 import ImagePreview from "../../../../../../reusable-ui/Admin/ImagePreview";
 import { getInputsConfig } from "../../../../../../../utils/admin/inputsConfig";
 import TextInput from "../../../../../../reusable-ui/TextInput";
+import EditInfoMessage from "./EditInfoMessage";
 
 export default function EditProduct() {
   const { productSelected, setProductSelected, handleEdit, titleEditRef } =
@@ -23,71 +24,54 @@ export default function EditProduct() {
 
   return (
     <EditProductStyled>
-      <div className="container-edit">
-        <ImagePreview
-          image={productSelected.imageSource}
-          title={productSelected.title}
-        />
-        <div className="input-fields">
-          {inputs.map(({ id, type, name, placeholder, value, Icon }) => (
-            <TextInput
-              key={id}
-              Icon={Icon}
-              type={type}
-              name={name}
-              value={value}
-              placeholder={placeholder}
-              onChange={handleChange}
-              className="input"
-              ref={name === "title" ? titleEditRef : null}
-            />
-          ))}
-        </div>
-        <span className="edit-text">
-          Cliquer sur un produit du menu pour le modifier
-          <span className="underline">en temps réel</span>
-        </span>
+      <ImagePreview
+        image={productSelected.imageSource}
+        title={productSelected.title}
+      />
+      <div className="input-fields">
+        {inputs.map(({ id, type, name, placeholder, value, Icon }) => (
+          <TextInput
+            key={id}
+            Icon={Icon}
+            type={type}
+            name={name}
+            value={value}
+            placeholder={placeholder}
+            onChange={handleChange}
+            className="input"
+            ref={name === "title" ? titleEditRef : null}
+          />
+        ))}
+        <EditInfoMessage />
       </div>
     </EditProductStyled>
   );
 }
 
 const EditProductStyled = styled.div`
-  .container-edit {
-    width: 80%;
+  width: 80%;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-column-gap: 20px;
+  margin-left: 54px;
+  margin-top: 35px;
+
+  /* INPUTS */
+  .icon {
+    margin: 0 15px 0 25px;
+  }
+
+  .input {
+    height: 35px;
+    margin: 0;
+    padding: 0;
+  }
+
+  .input-fields {
+    grid-area: 1 / 2 / 4 / -1;
     display: grid;
-    grid-template-columns: 1fr 3fr;
-    grid-column-gap: 20px;
-    margin-left: 54px;
-    margin-top: 35px;
-
-    /* INPUTS */
-    .icon {
-      margin: 0 15px 0 25px;
-    }
-
-    .input {
-      height: 35px;
-      margin: 0;
-      padding: 0;
-    }
-
-    .input-fields {
-      grid-area: 1 / 2 / 4 / -1;
-      display: grid;
-      grid-template-columns: 1fr;
-      grid-template-rows: repeat(4, 1fr);
-      grid-row-gap: 8px;
-    }
-
-    .edit-text {
-      grid-area: 3 / 2 / 4 / -1;
-      font-size: ${theme.fonts.size.SM};
-      color: ${theme.colors.primary};
-      .underline {
-        margin-left: 5px;
-        text-decoration: underline;
-      }
-    }
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(4, 1fr);
+    grid-row-gap: 8px;
   }
 `;
