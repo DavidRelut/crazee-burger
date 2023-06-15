7afa69dbde64c8cb1aa8ebc46927912019b79ea7
DavidR <codingaccelerator@gmail.com>
2023-06-15 16:19:35 +0200

    ✨ F13: FIREBASE  > users > Add function updateItem

--- src/hooks/useMenu.js
+++ src/hooks/useMenu.js
@@ -2,9 +2,10 @@ import { useState } from "react";
 import { fakeMenu } from "../fakeData/fakeMenu";
 import { deepClone, filter, findIndex } from "../utils/arrays";
 import { formatPrice, replaceFrenchCommaWithDot } from "../utils/maths";
+import { updateItem } from "../api/users";
 
 export const useMenu = () => {
-  const [menu, setMenu] = useState(fakeMenu.MEDIUM);
+  const [menu, setMenu] = useState(fakeMenu.LARGE);
 
   const handleAdd = (productToAdd) => {
     const menuDeepClone = deepClone(menu);
@@ -35,10 +36,11 @@ export const useMenu = () => {
     menuDeepClone[productToEdit] = editedProduct;
 
     setMenu(menuDeepClone);
+    updateItem("David", menuDeepClone);
   };
 
   const handleReset = () => {
-    setMenu(fakeMenu.MEDIUM);
+    setMenu(fakeMenu.LARGE);
   };
 
   return { menu, handleAdd, handleDelete, handleEdit, handleReset };
