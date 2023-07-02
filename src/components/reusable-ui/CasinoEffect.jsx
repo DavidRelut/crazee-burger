f42e64012c47dc917301462725354305dee76065
DavidR <codingaccelerator@gmail.com>
2023-07-02 18:22:27 +0200

    💄F14: BASKET > TOTAL > Add animation in Total price

--- src/components/reusable-ui/CasinoEffect.jsx
+++ src/components/reusable-ui/CasinoEffect.jsx
@@ -1,8 +1,47 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 import styled from "styled-components";
 
 export default function CasinoEffect({ className, count }) {
-  return <CasinoEffectStyled className={className}>{count}</CasinoEffectStyled>;
+  return (
+    <TransitionGroup component={CasinoEffectStyled}>
+      <CSSTransition classNames={"count-animated"} timeout={300} key={count}>
+        <span className={className}>{count}</span>
+      </CSSTransition>
+    </TransitionGroup>
+  );
 }
 
-const CasinoEffectStyled = styled.span``;
+const CasinoEffectStyled = styled.div`
+  position: relative;
+  overflow-y: hidden;
+  overflow-x: hidden;
+
+  span {
+    display: inline-block;
+  }
+
+  .count-animated-enter {
+    transform: translateY(100%);
+  }
+
+  .count-animated-enter-active {
+    transform: translateY(0%);
+    transition: 300ms;
+  }
+
+  .count-animated-enter-done {
+  }
+
+  .count-animated-exit {
+    transform: translateY(0%);
+    position: absolute;
+    right: 0;
+    bottom: 0;
+  }
+
+  .count-animated-exit-active {
+    transform: translateY(-100%);
+    transition: 300ms;
+  }
+`;
